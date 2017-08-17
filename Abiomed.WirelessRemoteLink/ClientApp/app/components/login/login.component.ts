a0b4abe8f8c0721499d67019ce32b2431ddb0118
Alessandro Agnello <aagnello@abiomed.com>
2017-08-17 15:19:29 -0400

    Updated CSS and Navigation

--- Abiomed.WirelessRemoteLink/ClientApp/app/components/login/login.component.ts
+++ Abiomed.WirelessRemoteLink/ClientApp/app/components/login/login.component.ts
@@ -5,7 +5,7 @@ import { Router} from '@angular/router';
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css'],
+    styleUrls: ['./login.component.css', '../../assets/css/AbiomedBase.css'],
     providers: [AuthenticationService]
 })
 
