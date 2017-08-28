1588dfead925460a9b16606efcc4494ab6195942
plemay <plemay@abiomed.com>
2017-08-28 13:29:22 -0400

    Pushing temp changes for Mail Server

---
+++ Abiomed.DotNetCore.MailConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Abiomed.DotNetCore.MailConsole
+{
+    class Program
+    {
+        static private Mail _mail;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Initialize();
+
+            _mail.SendEmailAsync(@"rlussier@abiomed.com", @"test",@"This is a simple validation/poc test").Wait();
+        }
+
+        static private void Initialize()
+        {
+            _mail = new Mail();
+        }
+    }
+}
